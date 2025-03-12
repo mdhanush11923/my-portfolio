0266f24de328151ac4d1539e86fcac2cc68c2442
M Dhanush <mdhanush11923@gmail.com>
2025-03-12 16:25:42 +0530

    add animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,24 +21,7 @@ export default function HeroPage() {
   }, []);
 
   return (
-    // <section
-    //   id="hero"
-    //   className="flex flex-col min-h-screen px-7 font-bold gap-2 justify-center"
-    // >
-    //   <h1 className="text-6xl text-blackblue">
-    //     Hi, my name is{" "}
-    //     <span className="bg-gradient-to-r from-primecolor to-secolor bg-clip-text text-transparent">
-    //       M Dhanush
-    //     </span>
-    //     <br />
-    //     I&apos;m the Developer you need.
-    //   </h1>
-    //   <p className="hero-cta load-hidden">
-    //     <a rel="noreferrer" className="cta-btn cta-btn--hero" href="#about">
-    //       Know more
-    //     </a>
-    //   </p>
-    // </section>
+
     <div>
       <Topbar />
       <div id="top"></div>
